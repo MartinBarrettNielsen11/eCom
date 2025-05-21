ee51e7fe51f2e0b38995254e1f4e7ca4427fd6c6
Martin Daniel Nielsen <martindanielnielsen96@gmail.com>
2025-05-21 20:32:23 +0200

    init attempt to split order creation in seperate handler

--- src/Application/CommandHandlers/CreateOrderCommandHandler.cs
+++ src/Application/CommandHandlers/CreateOrderCommandHandler.cs
@@ -1,7 +1,32 @@
-﻿namespace Service.CommandHandlers;
+﻿using Application.Results;
+using Contracts.Events;
+using MassTransit;
+using MediatR;
 
-public record CreateOrderCommand(Guid OrderId, int CustomerId, DateTime OrderDate);
-public class CreateOrderCommandHandler
+namespace Service.CommandHandlers;
+
+public record CreateOrderCommand(Guid OrderId, int CustomerId, DateTime OrderDate) : IRequest<CommandResult<EmptyResult>>;
+
+public class CreateOrderCommandHandler(IPublishEndpoint publishEndpoint, IOrderService orderService) 
+    : IRequestHandler<CreateOrderCommand, CommandResult<EmptyResult>>
 {
-    
+    public async Task<CommandResult<EmptyResult>> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        var createdOrder = await orderService.CreateOrder(order, cancellationToken);
+
+        await publishEndpoint.Publish(new OrderCreated()
+        {
+            CreatedAt = createdOrder.OrderDate, 
+            Id = createdOrder.Id,
+            OrderId = createdOrder.OrderId,
+            TotalAmount = createdOrder.OrderItems.Sum(p => p.Price * p.Quantity)
+        }, context =>
+        {
+            context.Headers.Set("header-v1", "header-v1-value");
+            context.TimeToLive = TimeSpan.FromSeconds(30);
+        }, cancellationToken);
+        
+        
+        return _requestHandlerImplementation.Handle(request, cancellationToken);
+    }
 }
