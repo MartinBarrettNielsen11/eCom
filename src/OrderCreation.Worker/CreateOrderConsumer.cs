b213ef1ebccf93aad757448d5fa64f199f673b9f
Martin Daniel Nielsen <martindanielnielsen96@gmail.com>
2025-06-15 03:43:41 +0200

    setup new mass transit worker for decoupled order creation

---
+++ src/OrderCreation.Worker/CreateOrderConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Contracts.Models;
+using Management.Application;
+using MassTransit;
+
+namespace OrderCreation.Worker;
+
+public class CreateOrderConsumer(IOrderService orderService) : IConsumer<OrderModel>
+{
+
+    public async Task Consume(ConsumeContext<OrderModel> context)
+    {
+        Console.WriteLine($"I got a command to create an order: {context.Message}");
+        await Task.CompletedTask;
+    }
+}
---
+++ src/OrderCreation.Worker/Program.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Threading.Tasks;
+using Management.Application;
+using Microsoft.Extensions.Hosting;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OrderCreation.Worker
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((_, services) =>
+                {
+
+                    services.AddScoped<IOrderRepository, OrderRepository>();
+                    services.AddScoped<IOrderService, OrderService>();
+                    
+                    services.AddMassTransit(x =>
+                    {
+                        var entryAssembly = Assembly.GetEntryAssembly();
+                        x.AddConsumers(entryAssembly);
+                        x.UsingRabbitMq((context, cfg) => { cfg.ConfigureEndpoints(context); });
+                    });
+                });
+    }
+}
