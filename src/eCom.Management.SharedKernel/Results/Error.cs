6b1a1a0d811705496e41b6ac261d46da0515f083
MartinBarrettNielsen11 <martindanielnielsen96@gmail.com>
2025-09-05 23:27:54 +0200

    clean up duplicate declarations in .editorconfig

--- src/eCom.Management.SharedKernel/Results/Error.cs
+++ src/eCom.Management.SharedKernel/Results/Error.cs
@@ -14,24 +14,17 @@ public record Error
     }
     
     public static readonly Error None = new(
-        string.Empty, 
-        string.Empty, 
+        string.Empty,
+        string.Empty,
         ErrorType.Failure);
-    
+
     public static readonly Error NullValue = new(
         "General.Null",
         "Null value was provided",
         ErrorType.Failure);
 
-    public static Error Failure(string code, string description) =>
-        new(code, description, ErrorType.Failure);
-
-    public static Error NotFound(string code, string description) =>
-        new(code, description, ErrorType.NotFound);
-
-    public static Error Problem(string code, string description) =>
-        new(code, description, ErrorType.Problem);
-
-    public static Error Conflict(string code, string description) =>
-        new(code, description, ErrorType.Conflict);
+    public static Error Failure(string code, string description) => new(code, description, ErrorType.Failure);
+    public static Error NotFound(string code, string description) => new(code, description, ErrorType.NotFound);
+    public static Error Problem(string code, string description) => new(code, description, ErrorType.Problem);
+    public static Error Conflict(string code, string description) => new(code, description, ErrorType.Conflict);
 }
